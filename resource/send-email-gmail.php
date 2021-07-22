7881e928e9463581826de72d1404b2ca11a9d7be
HAPPY-CHANDRU-RAJU <rajuashwin1410@gmail.com>
2021-07-22 12:53:33 +0530

    mail Updated

---
+++ resource/send-email-gmail.php
@@ -0,0 +1,26 @@
+<?php
+require_once "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+$mail = new PHPMailer(true);
+$mail->IsSMTP();
+$mail->Mailer = 'smtp';
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+
+$mail->SMTPDebug = 0;  
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+
+//auth credential information
+$mail->Username = "no.reply.happie@gmail.com";
+$mail->Password = "D4ba23a1";
+
+$mail->IsHTML(true);
+$mail->SingleTo = true;
+
+$mail->setFrom('no.reply.happie@gmail.com', 'AR Builders - Website Bot'); #your email id
